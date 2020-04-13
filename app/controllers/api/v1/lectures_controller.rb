75aa4e6e182a605c7cc05a3636398f4d0928a9f0
KIguchi <kiguchi118@gmail.com>
2020-04-13 14:16:33 +0900

    [Add]検索場合分け，部分一致検索

--- app/controllers/api/v1/lectures_controller.rb
+++ app/controllers/api/v1/lectures_controller.rb
@@ -3,7 +3,32 @@ module Api
     class Api::V1::LecturesController < ApplicationController
 
       def index
-        @subjects = Subject.where("title = ?", params[:keyword])
+        teacher_name = params[:teacher_name]
+        subject_title = params[:keyword]
+        if teacher_name.present?
+          @subjects = Subject.
+                        joins(:teacher).
+                        where("teachers.name like ?", "%#{teacher_name}%")
+          if subject_title.present?
+            @subjects = @subjects.where("subjects.title like ?", "%#{subject_title}%")
+          end
+        elsif subject_title.present?
+          @subjects = Subject.where("title like ?", "%#{subject_title}%")
+        end
+
+        # if params[:keyword].present? && params[:teacher_name].present?
+        #   @subjects = Subject.
+        #                 joins(:teacher).
+        #                 where("(teachers.name = ?) AND (subjects.title = ?)",
+        #                       params[:teacher_name], params[:keyword])
+        # elsif params[:keyword].present?
+        #   @subjects = Subject.where("title = ?", params[:keyword])
+        # else
+        #   @subjects = Subject.
+        #                 joins(:teacher).
+        #                 where("teachers.name = ?", params[:teacher_name])
+        # end
+
         render json: @subjects.to_json(
           except: [:teacher_id, :created_at, :updated_at],
           include: [
